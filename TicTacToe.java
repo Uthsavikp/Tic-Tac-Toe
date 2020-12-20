657c1735244c65210676d9a29a241a499cf6526b
uthsa <uthsavi29@gmail.com>
2020-12-20 14:58:56 +0530

    Added:Create A Tic Tac Toe Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,14 @@
 public class TicTacToe {
+    public static char[] createBoard() {
+        char[] board = new char[10];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = ' '; //initializing empty values
+        }
+        return board;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
+        char[] board = createBoard();
     }
 }
